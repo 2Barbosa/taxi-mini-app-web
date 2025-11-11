ca4e8f48326474e98a62ac62424ca69037e2b00a
2Barbosa <fedorovba3@gmail.com>
2025-11-11 11:43:18 +0900

    Update UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4176947633"
+    serviceWorkerVersion: "1184566356"
   }
 });
