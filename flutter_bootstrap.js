6a792c46f10299ac46c3fe0e502c390894c4d8f9
2Barbosa <fedorovba3@gmail.com>
2025-11-12 17:59:58 +0900

    Update UI + Supabase

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1184566356"
+    serviceWorkerVersion: "1453510678"
   }
 });
